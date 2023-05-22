5dfd4f3fe7b2b1bf5c8a0b9934c9754db9ef7e78
Manon <manonlefkos@gmail.com>
2023-05-22 14:52:59 +0200

    update api link to include forecast for 7 days

--- src/api-handler.js
+++ src/api-handler.js
@@ -1,15 +1,15 @@
 async function getCurrentWeather(location) {
-  const locationLink = `https://api.weatherapi.com/v1/current.json?key=51ca3ed754014c58aad194423231805&q=${location}&aqi=no`;
+  const locationLink = `https://api.weatherapi.com/v1/forecast.json?key=51ca3ed754014c58aad194423231805&q=${location}&days=7&aqi=no&alerts=no`;
   const response = await fetch(locationLink, { mode: "cors" });
   const currentWeather = await response.json();
-  return currentWeather
+  console.log(currentWeather);
+  return currentWeather;
 }
 
 async function getCurrentTemperature(location) {
-  const data = await (getCurrentWeather(location))
-  // const currentTemperature = data.
+  const data = await getCurrentWeather(location);
   console.log(`temp in C: ${data.current.temp_c}`);
-} 
+}
 
 getCurrentTemperature("Amsterdam");
 console.log("hi");
