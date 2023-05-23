772615515c5546ca163ba94f79df6f6eba80112f
Manon <manonlefkos@gmail.com>
2023-05-23 23:11:57 +0200

    add function to return current weather object

--- src/api-handler.js
+++ src/api-handler.js
@@ -1,15 +1,30 @@
-async function getCurrentWeather(location) {
+let tempUnit = "c"
+
+async function getWeatherData(location) {
   const locationLink = `https://api.weatherapi.com/v1/forecast.json?key=51ca3ed754014c58aad194423231805&q=${location}&days=7&aqi=no&alerts=no`;
   const response = await fetch(locationLink, { mode: "cors" });
-  const currentWeather = await response.json();
-  console.log(currentWeather);
-  return currentWeather;
+  const weatherData = await response.json();
+  console.log(weatherData);
+  return weatherData;
 }
 
-async function getCurrentTemperature(location) {
-  const data = await getCurrentWeather(location);
-  console.log(`temp in C: ${data.current.temp_c}`);
+async function getCurrentWeather(location) {
+  const data = await getWeatherData(location);
+  const currentData = {
+    location: data.location.name,
+    weather: data.current.condition.text,
+    temperature: (function temperature() {
+      if (tempUnit === "c") {
+        return `${data.current.temp_c}°C`;
+      }
+      return `${data.current.temp_f}°F`
+    })()
+    }
+  console.log(currentData);
+  return currentData;
 }
 
-getCurrentTemperature("Amsterdam");
+getCurrentWeather("Amsterdam");
 console.log("hi");
+
+
