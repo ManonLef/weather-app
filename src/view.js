e29d5ed86fc87c4f847c413abbd84dd8e28b9efb
Manon <manonlefkos@gmail.com>
2023-05-28 16:04:55 +0200

    clean up more dynamically rendered content and place in html

--- src/view.js
+++ src/view.js
@@ -1,26 +1,29 @@
 /* eslint no-use-before-define: ["error", { "functions": false }] */
 
-// app Header:
+// querySelectors
 const toggle = document.querySelector("#temp-toggle");
 const currentWeather = document.querySelector(".current-container")
 const inputLocation = document.querySelector("#location-input");
 const submitLocation = document.querySelector("button");
 const errorDiv = document.querySelector(".error");
+const forecastWeatherHeader = document.querySelector(".forecast-header");
+const forecastWeather = document.querySelector(".forecast-days");
 
+
+// populates the current weather container
 async function renderCurrent(wx) {
-  // this data still has to go to the api-handler
   const locationWeather = await wx;
   const current = locationWeather[0];
-  //
+
   clearCurrent();
-  // header
+
   const currentHeader = document.createElement("div");
   currentHeader.className = "current-header";
   currentHeader.textContent = `Current Weather in ${current.location}, ${current.country}`;
-  // img
+
   const icon = new Image();
   icon.src = `${current.icon}`;
-  //
+
   const date = document.createElement("div");
   date.className = "updated";
   date.textContent = `Updated: ${current.day} ${current.date} ${current.last_update_time}`;
@@ -33,15 +36,7 @@ async function renderCurrent(wx) {
   elementsToRender.forEach((element) => currentWeather.append(element));
 }
 
-// forecast weather display
-const forecastContainer = document.querySelector(".forecast-container");
-const forecastWeatherHeader = document.createElement("div");
-forecastWeatherHeader.className = "forecast-header";
-const forecastWeather = document.createElement("div");
-forecastWeather.className = "forecast-days";
-forecastContainer.append(forecastWeatherHeader, forecastWeather);
-
-
+// populates the forecast days container
 async function renderFuture(wx) {
   const locationWeather = await wx;
   const forecast = locationWeather[1];
@@ -82,15 +77,16 @@ async function renderFuture(wx) {
     ];
 
     elementsToRender.forEach((element) => forecastCard.append(element));
-
     forecastWeather.appendChild(forecastCard);
   });
 }
 
+// clear location input field upon submit
 function clearInput() {
   inputLocation.value = "";
 }
 
+// clear appended weather functions
 function clearCurrent() {
   errorDiv.textContent = ""
   while (currentWeather.firstChild) {
